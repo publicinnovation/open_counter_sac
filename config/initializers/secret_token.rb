df7d97a2dbd68c9ac85774fa38130e59a3132c88
Thomas Hopkins <hopkins.tc@gmail.com>
2013-05-31 21:20:13 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenCounterSac::Application.config.secret_token = '7fcddb4a75347438f20d43611db6fb742dde0091ab64f76b7eef64d947f3c5ba8bc9b3ed6be6b3620a41ca673cb6cf3174ec864d4e718368484ca24f8ff9052c'
