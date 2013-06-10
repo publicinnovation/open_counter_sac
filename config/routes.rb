a17c56986c19c8e298f2c9b1546bd54c4cca3293
Thomas Hopkins <hopkins.tc@gmail.com>
2013-06-10 00:07:31 -0700

    add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 OpenCounterSac::Application.routes.draw do
   devise_for :users
 
+  root :to => 'high_voltage/pages#show', :id => 'welcome'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
